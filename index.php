82a3270853533adf8b0ff8f19e013e0c79b1a2d8
RMG8 <56052470+RahelMaier@users.noreply.github.com>
2020-01-26 16:30:26 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WEB120 Portal - Edit this code to make it your own!</title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+<link rel="stylesheet" href="css/nav.css" />
+<link rel="stylesheet" href="css/portal.css" />
+<link rel="stylesheet" href="css/form.css" />
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<main class="wrapper">
+<header>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i> (Rahel Maier SCC WEB120 Portal</a></h1>
+  <nav>
+    <ul class="topnav" id="myTopnav">
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart/Layout</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contactme.php">Contact Sara</a></li>
+      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    </ul>
+  </nav>
+</header>
+        
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/20180816_191508.jpg" class="desktop" alt="" />
+ <img src="images/GDP_1357.jpg" class="phone" alt="" />
+ <p>I was born in <strong>Ethiopiaand was adopted by my family, coming home to Stanwood, Washington at a young age</strong> . I still have vivid memories of Ethiopia: I remember being moved from orphanage to orphanage, always searching for “home” but never finding it. I remember thinking to myself, why do I keep getting sent somewhere new? My parents had died  when i was only four years old and there wasn’t a legal guardian to care for me. My aunt was very young and wasn't able to provide for both of us. It must have been a difficult decision for her to send me to an orphanage way far from where she was. I still ask myself, why do bad things happen to good people? Being four and losing my parents was enormous and devastating. It left a hollow place inside me, something I’ve had trouble filling while learning to establish a new identity in a whole new family. </p>
+ <p>  When I came to the United States, it was a new world. From the moment I boarded the plane to the moment I stepped on American soil, everything seemed unreal. I was happy to have a new family, but I was also scared. Having a family was important to me. As a child, I loved the time spent with my family. I was also mesmerized by fashion i also loved playing with neighborhood kids from a young age, and I remember knocking on the neighbors doors early morning to waking  kids up so we can get out and play. Later on the day ur whole families would come watch us play. This brought our families closer.</p>
+ <p>In Ethiopia, having a television meant that you were rich. When I first arrived in <em>Stanwood, I searched for a television and movies. During my teen years, </em>I thought hip-hop and rap culture was cool. I remember blasting the speakers and listing to songs. I had no idea what they were about, I just loved the sound and being cool and fitting in. I ended up having a conflict with my parents about the content of the music. I had some hardships during in school. In 2009, when I was in tenth grade, I was given the opportunity to move to new york and join a vogue . However, I didn't end up going  because i was under age and my parents were religious people which made it easy to say no to me. I become angry child it felt as my dream was taken away from me. Then when school started, my life wasn't so charming at home. I was going through turmoil internally and externally. I got in trouble, didn’t do my homework, and made bad choices. I lost hope for school. But because of my parents didn't gave up and kept the support sent me to counselling sessions , I began to think positively; if it wasn't for them but my own passion to wanting to succeed in life, even though I don't know where I would be right then. </p>
+ <p>My junior year I focused intently on my academics. My GPA rose from a C to a B-. I can say now that I learned a great lesson from failures. I learned that with hard work and a goal, I can do anything I set my mind to. The experiences I faced shaped me into the person I am now: a hardworking and focused young women. 
+</p>
+     <p>
+                As I look back and think about where life has taken me, I know I’ve come far through many challenges. Though life is hard, with determination and hard work, it can get better. I see myself as a new women, thanks to the friends, family members and mentors in my life. I am becoming a well-educated young women, with the skills and passion to give back to my community. 
+
+            </p>
+
+            <p>
+                Life has forced me to grow up. I see things in a new light. My resilience was tested, and I found the dedication and commitment to reach my full potential. As I battled through high school, I always found myself using my family as a blueprint to build me into the person I am today. If I could have only one family for the rest of my life, I would choose this family; they have taught me lessons that I will never forget as long as I live. 
+
+            </p>
+
+            <p>
+                I made a plan to pursue a higher education and not settle for less. By working hard i discovered my passion for work, and the importance of education. While my friends honed their wakeboarding skills, I discovered my passion for work and education. I never heisted because every night I gazed into a set of the proudest eyes before I went to bed. I remember that several weeks after my parents attended my high school graduation, I had the honor of congratulating myself and my family. I realized that one chapter of my education is done and my dream after was to get a college diploma. A college where I can strive for an academic excellence, academic curiosity, engagement, and an environment that nurtures each person's potential. I wanted to start in a small community where I could make the most impact, with people whom I believe can make have the same value in the community as well, such as mentoring kids at risk. My adoptive parents' college diploma reminds me that, no matter how bleak a situation may appear, I have the power to better it through diligence.  I intend to use my education to help others in need, such as minorities that need help in getting out of poverty and seek a higher education. I am from Ethiopia so I know what it feels like being in poverty and not knowing when and where I will get my next meal.
+
+            </p>
+
+            <p>
+                Even tough life doesn't always work out as planned, I have learned to move forward and find my next calling.
+            </p>
+            <p>
+                My plane is to get my degree as a computer programmer here at seattle college!  
+
+            </p>
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<!-- START Footer -->
+<footer>
+  <p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php" target="_blank">Put your name here - not Sara's you will loose points if it's Sara!</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+</main>
+<!-- END WRAPPER -->
+
+    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+    <script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+     </script>
+</body>
+</html>
